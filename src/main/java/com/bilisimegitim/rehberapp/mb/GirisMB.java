7f55cbdb456345450e012fe266742101bfc03e7c
BemDell1 <BemDell1@DESKTOP-6OQP37J>
2018-07-07 13:28:40 +0300

    girisFacade ve girisMB düzenlendi

--- src/main/java/com/bilisimegitim/rehberapp/mb/GirisMB.java
+++ src/main/java/com/bilisimegitim/rehberapp/mb/GirisMB.java
@@ -1,5 +1,7 @@
 package com.bilisimegitim.rehberapp.mb;
 
+import com.bilisimegitim.rehberapp.session.GirisFacade;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 
@@ -7,7 +9,41 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class GirisMB {
 
+    @EJB
+    private GirisFacade girisFacade;
+
+    private String kullanici;
+    private String sifre;
+
     public GirisMB() {
+    }
+
+    public String getKullanici() {
+        return kullanici;
+    }
+
+    public void setKullanici(String kullanici) {
+        this.kullanici = kullanici;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+
+    public String girisKontrol() {
+
+        boolean sonuc = girisFacade.girisKontrol(kullanici, sifre);
+
+        if (sonuc) {
+            return "menu.xhtml";
+        } else {
+            return "";
+        }
+
     }
 
 }
